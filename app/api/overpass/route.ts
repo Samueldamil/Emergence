e9d5909222b275db09157ef96e28036e4692ec42
Samueldamil <damilareedun.com>
2026-05-31 05:39:11 +0100

    Fetch error fix

--- app/api/overpass/route.ts
+++ app/api/overpass/route.ts
@@ -4,15 +4,20 @@ export async function POST(req: Request) {
     try {
         const query = await req.text();
 
-        const response = await fetch("https://overpass-api.de/api/interpreter", {
+        const response = await fetch("https://overpass.private.coffee/api/interpreter", {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
+                "User-Agent": "Emergency-App",
             },
             body: `data=${encodeURIComponent(query)}`,
         });
 
+        console.log(response.status);
+
         const text = await response.text();
+
+        console.log(text);
         return new NextResponse(text, {
             status: response.status,
             headers: {
